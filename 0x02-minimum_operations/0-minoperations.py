643a360ee730e72a217356d7ce485551d3350287
Emmanuel Lafenwa <leoemaxie@gmail.com>
2024-06-14 02:48:51 +0100

    minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result in exactly n
+    H characters in the file, given a number n.
+    """
+    no_of_operations = 0
+    no_of_characters = 0
+    operation = "COPY"
+
+    if no_of_characters == n:
+        return no_of_operations
+    elif  no_of_characters > n:
+        return 0
+    # else:
+    #     if step == "COPY":
+            
+    #     if step == "PASTE":
+        
+    #     op += 1
+    #     minOperations(n)
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,9 @@
+0x02 Minimum Operations
+
+## Overview
+
+Algorithm to find the fewest number of operationa for a file editor.
+
+## Link
+
+[Minimum Operations](https://intranet.alxswe.com/projects/1427)
